5d95bd55d87e688412359bdd16dbe1c01db8bc6b
DNAFun <55844319+DNAFun@users.noreply.github.com>
2022-04-27 16:58:24 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -22,7 +22,7 @@ dd if=/dev/zero of=10mb.bin bs=10M count=1
 /usr/local/bin/v2ray -config /usr/local/etc/v2ray/config.json &
 # Run nginx
 /bin/bash -c "envsubst '\$PORT,\$WS_PATH' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf" && nginx -g 'daemon off;'
-/bin/bash sudo chown -R www-data:www-data /var/log/nginx;
-/bin/bash sudo chmod -R 755 /var/log/nginx;
+/bin/bash chown -R www-data:www-data /var/log/nginx;
+/bin/bash chmod -R 755 /var/log/nginx;
 
-/bin/bash nginx -s reload
+/bin/bash -c "envsubst '\$PORT,\$WS_PATH' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf" && nginx -s reload
